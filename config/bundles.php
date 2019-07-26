4e6e43c8c638e49c692ea1cdf55c6334ea7bc721
Marcin Malessa <marcin@malessa.pl>
2019-07-26 21:25:00 +0200

    Composer update

--- config/bundles.php
+++ config/bundles.php
@@ -2,6 +2,5 @@
 
 return [
     Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
-    Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle::class => ['all' => true],
     Prooph\Bundle\EventStore\ProophEventStoreBundle::class => ['all' => true],
 ];
