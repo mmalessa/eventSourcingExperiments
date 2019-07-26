14e2d449db99bc438bd79be99f96824be9e205cb
Marcin Malessa <marcin@malessa.pl>
2019-07-26 21:05:47 +0200

    Prooph installed

--- config/bundles.php
+++ config/bundles.php
@@ -3,4 +3,5 @@
 return [
     Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
     Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle::class => ['all' => true],
+    Prooph\Bundle\EventStore\ProophEventStoreBundle::class => ['all' => true],
 ];
